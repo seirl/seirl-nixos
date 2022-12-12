1e5fe3d1bc02bcdd882bb68f4fd99e2a1f41780f
Antoine Pietri <antoine.pietri1@gmail.com>
2022-12-13 00:25:49 +0100

    Move ecryptfs-specific packages in the right role

--- roles/ecryptfs.nix
+++ roles/ecryptfs.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    ecryptfs
+    ecryptfs-helper
+  ];
+
   boot.kernelModules = [ "ecryptfs" ];
   boot.supportedFilesystems = [ "ecryptfs" ];
 }
--- roles/nvidia.nix
+++ roles/nvidia.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ...}:
 
 {
-  environment.systemPackages = with pkgs; [
-    ecryptfs
-    ecryptfs-helper
-  ];
-
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 }
