785ac908385d96ce231c200a63fa27054803060f
Antoine Pietri <antoine.pietri1@gmail.com>
2023-05-01 21:45:23 +0200

    Add cuda-maintainers cachix

--- roles/nvidia.nix
+++ roles/nvidia.nix
@@ -11,5 +11,14 @@ in {
     nixpkgs.config.cudaSupport = true;
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    nix.settings = {
+      substituters = [
+        "https://cuda-maintainers.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
+    };
   };
 }
