5c3b475a9e62677c1f9eed09cce011c0c640521a
Antoine Pietri <antoine.pietri1@gmail.com>
2023-05-20 17:49:22 +0200

    flake: reformat

--- flake.nix
+++ flake.nix
@@ -10,49 +10,55 @@
       inputs.nixpkgs.follows = "/nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
-  {
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
     # Colmena hive output
-    colmena = let
-      machines = import ./machines;
-    in
-    {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = "x86_64-linux";
+    colmena =
+      let
+        machines = import ./machines;
+      in
+      {
+        meta = {
+          nixpkgs = import nixpkgs {
+            system = "x86_64-linux";
+          };
         };
-      };
-      defaults = { pkgs, ... }: {
-        imports = [
-          home-manager.nixosModules.home-manager
-          ./common
-        ];
+        defaults = { pkgs, ... }: {
+          imports = [
+            home-manager.nixosModules.home-manager
+            ./common
+          ];
 
-        # deployment.replaceUnknownProfiles = true;
-        deployment.allowLocalDeployment = true;
+          # deployment.replaceUnknownProfiles = true;
+          deployment.allowLocalDeployment = true;
 
-        home-manager.useUserPackages = true;
-        home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.useGlobalPkgs = true;
 
-        system.stateVersion = "22.11";
-      };
-    } // machines;
+          system.stateVersion = "22.11";
+        };
+      } // machines;
 
     # Home configurations.
     # This exposes the home configurations of each user@host pair directly by
     # extracting them from the generated colmena hive config. This allows
     # non-NixOS nodes to apply their home-manager config locally.
-    homeConfigurations = let
-      hive = (inputs.colmena.lib.makeHive self.colmena);
-    in with nixpkgs.lib; listToAttrs (
-      flatten (
-        mapAttrsToList (machine: machineConfig: (
-          mapAttrsToList (user: homeConfig: (
-            {name = "${user}@${machine}"; value = homeConfig;}
-          )) machineConfig.config.home-manager.users
-        )) hive.nodes
-      )
-    );
+    homeConfigurations =
+      let
+        hive = (inputs.colmena.lib.makeHive self.colmena);
+      in
+      with nixpkgs.lib; listToAttrs (
+        flatten (
+          mapAttrsToList
+            (machine: machineConfig: (
+              mapAttrsToList
+                (user: homeConfig: (
+                  { name = "${user}@${machine}"; value = homeConfig; }
+                ))
+                machineConfig.config.home-manager.users
+            ))
+            hive.nodes
+        )
+      );
 
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
