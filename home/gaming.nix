e50da5df4b829cae5716da98618c379190a7abe5
Antoine Pietri <antoine.pietri1@gmail.com>
2023-12-22 17:35:00 +0100

    home/gaming: refactor into wow/bnet wine wrappers

--- home/gaming.nix
+++ home/gaming.nix
@@ -18,6 +18,40 @@ let
   bnetWinePrefix = "${config.xdg.dataHome}/wineprefixes/battlenet";
   bnetWowPath = "${bnetWinePrefix}/drive_c/Program Files (x86)/World of Warcraft";
   bnetPath = "${bnetWinePrefix}/drive_c/Program Files (x86)/Battle.net";
+
+  wowWineWrapper = (pkgs.writeShellScriptBin "wow-wine-wrapper" ''
+    set -e
+    export WINEARCH=win64
+    export WINEESYNC=1
+    export WINEPREFIX="${wowWinePrefix}"
+    ${pkgs.winetricks}/bin/winetricks dxvk
+    mkdir -p "${wowSharedPath}"
+    mkdir -p $( dirname "${wowPath}" )
+    ln -sfn "${wowSharedPath}" "${wowPath}"
+    exec $*
+  '');
+
+  bnetWineWrapper = (pkgs.writeShellScriptBin "battlenet-wine-wrapper" ''
+    set -e
+    export WINEARCH=win64
+    export WINEESYNC=1
+    export WINEPREFIX="${bnetWinePrefix}"
+    ${pkgs.winetricks}/bin/winetricks dxvk
+    ${pkgs.winetricks}/bin/winetricks corefonts
+    mkdir -p "${bnetSharedPath}"
+    mkdir -p "${wowSharedPath}"
+    mkdir -p $( dirname "${bnetPath}" )
+    mkdir -p $( dirname "${bnetWowPath}" )
+    ln -sfn "${bnetSharedPath}" "${bnetPath}"
+    ln -sfn "${wowSharedPath}" "${bnetWowPath}"
+    exec $*
+  '');
+
+  wowStartScript = (pkgs.writeShellScriptBin "wow-start-script" ''
+    ${wowBackupScript}
+    ${winePkg}/bin/wine64 "${wowPath}/_${wowFlavor}_/Wow.exe"
+    ${wowBackupScript}
+  '');
 in
 {
   options = {
@@ -65,33 +99,17 @@ in
 
     home.packages = [
       (pkgs.writeShellScriptBin "wow" ''
-        set -e
-        export WINEARCH=win64
-        export WINEESYNC=1
-        export WINEPREFIX="${wowWinePrefix}"
-        ${pkgs.winetricks}/bin/winetricks dxvk
-        mkdir -p "${wowSharedPath}"
-        mkdir -p $( dirname "${wowPath}" )
-        ln -sfn "${wowSharedPath}" "${wowPath}"
-        ${wowBackupScript}
-        ${winePkg}/bin/wine64 "${wowPath}/_${wowFlavor}_/Wow.exe"
-        ${wowBackupScript}
+        ${wowWineWrapper}/bin/wow-wine-wrapper ${wowStartScript}/bin/wow-start-script
       '')
 
       (pkgs.writeShellScriptBin "battlenet" ''
-        set -e
-        export WINEARCH=win64
-        export WINEESYNC=1
-        export WINEPREFIX="${bnetWinePrefix}"
-        ${pkgs.winetricks}/bin/winetricks dxvk
-        ${pkgs.winetricks}/bin/winetricks corefonts
-        mkdir -p "${bnetSharedPath}"
-        mkdir -p "${wowSharedPath}"
-        mkdir -p $( dirname "${bnetPath}" )
-        mkdir -p $( dirname "${bnetWowPath}" )
-        ln -sfn "${bnetSharedPath}" "${bnetPath}"
-        ln -sfn "${wowSharedPath}" "${bnetWowPath}"
-        ${winePkg}/bin/wine64 "${bnetPath}/Battle.net.exe"
+        ${bnetWineWrapper}/bin/battlenet-wine-wrapper ${winePkg}/bin/wine64 "${bnetPath}/Battle.net.exe"
+      '')
+
+      (pkgs.writeShellScriptBin "battlenet-setup" ''
+        bnet_setup=$( mktemp --tmpdir Battle.net-Setup.XXXXXXXXXX.exe )
+        ${pkgs.wget}/bin/wget -O "$bnet_setup" 'https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe'
+        ${bnetWineWrapper}/bin/battlenet-wine-wrapper ${winePkg}/bin/wine64 "$bnet_setup"
       '')
     ];
   };
