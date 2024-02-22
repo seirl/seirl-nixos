d7b96761ea01968299b6061934b75b46efc9d51c
Antoine Pietri <antoine.pietri1@gmail.com>
2024-02-22 09:10:33 +0100

    seirl/ssh: add corp USB keys

--- common/users.nix
+++ common/users.nix
@@ -10,7 +10,8 @@ rec {
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILiua8eEg+nU0XSbYPTgnOMftzvpbN+u7v5jDabeO/0E seirl"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEbuYBUcnE4DKIl8aJhBr3WDKwefZLBgoATyMxu2ISRy seirl@google"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBI/JwDTVgDieMCQz8Pe9GejJDed5hJnGuPo5Yer9kooLNS6qJSvYVKNxAK3n2P5Ftr5dfMSlZD56dVM37nI8q2o= seirl@google-small-usbc"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMSFbO4Wxz5Yie7rjfziXOSaY7vodWAvpfw9SlgCbZcwfKPTjEpjd/FRMJEAAjv27DumofczSHURcQRrd6SBTl4= seirl@google-keyring-usbc"
     ];
   };
 
