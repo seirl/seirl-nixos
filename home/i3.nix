932175eb4e0c2f4f870cb5ed18503b781a9214d8
Antoine Pietri <antoine.pietri1@gmail.com>
2024-09-21 18:47:26 +0200

    i3: add defaultWorkspace option

--- home/i3.nix
+++ home/i3.nix
@@ -18,6 +18,7 @@ in
 
       config = {
         modifier = "Mod4";
+        defaultWorkspace = "workspace number 1";
         keybindings = lib.mkOptionDefault {
           # Workspace moves
           "${mod}+Ctrl+Left" = "move workspace to output left";
