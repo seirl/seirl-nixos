5f9be38f483d0330f92f1aa5850557451f0fa0d3
Antoine Pietri <antoine.pietri1@gmail.com>
2025-06-14 15:53:57 +0200

    enable avahi in gaming profile

--- roles/gaming.nix
+++ roles/gaming.nix
@@ -39,6 +39,8 @@ in
       capSysAdmin = true;
       openFirewall = true;
     };
+    services.avahi.enable = true;
+    services.avahi.publish.userServices = true;
 
     # Steam games SSL fix.
     #
