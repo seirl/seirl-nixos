187056e9474af05b5c1edd53aa0410ff2b5ba996
Antoine Pietri <antoine.pietri1@gmail.com>
2025-09-06 19:14:05 +0200

    Fix renamed vscode options.

--- home/vscode.nix
+++ home/vscode.nix
@@ -11,7 +11,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      userSettings = {
+      profiles.default.userSettings = {
         "keyboard.dispatch" = "keyCode";
         "window.zoomLevel" = -1;
       };
