d4dd87cda506ea1d09320eee8ffbcfb7632a272d
Antoine Pietri <antoine.pietri1@gmail.com>
2025-09-13 13:39:34 +0200

    zsh: use initContent instead of deprecated initExtra*

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,13 +1,10 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   config = {
     programs.zsh = {
       enable = true;
       syntaxHighlighting.enable = true;
-      initExtraFirst = ''
-        source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
-      '';
 
       shellAliases = {
         ip = "ip --color=auto";
@@ -17,15 +14,20 @@
         sizes = "zsh -c \"p=\${1:-.}; echo \$p; du -sh \$p/*(N) \$p/.*(N) | sort -h\"";
       };
 
-      initExtra = ''
-        # Bind Alt+m to copy the previous word.
-        autoload -Uz copy-earlier-word
-        zle -N copy-earlier-word
-        bindkey "^[m" copy-earlier-word
+      initContent = lib.mkMerge [
+        (lib.mkBefore ''
+          source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+        '')
+        (lib.mkAfter ''
+          # Bind Alt+m to copy the previous word.
+          autoload -Uz copy-earlier-word
+          zle -N copy-earlier-word
+          bindkey "^[m" copy-earlier-word
 
-        # Disable stupid ^S and ^Q freeze
-        stty -ixon
-      '';
+          # Disable stupid ^S and ^Q freeze
+          stty -ixon
+        '')
+      ];
     };
   };
 }
