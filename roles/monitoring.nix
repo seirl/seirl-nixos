1550f21e2a71e1b50f0b7aed57a6980a51421899
Antoine Pietri <antoine.pietri1@gmail.com>
2025-10-25 20:04:48 +0200

    grafana: use new option to refer to http_port

--- roles/monitoring.nix
+++ roles/monitoring.nix
@@ -69,7 +69,7 @@ in
       enableACME = true;
 
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
       };
     };
   };
