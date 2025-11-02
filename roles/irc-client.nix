ab59f7cd533c0634d18be1beeb745bbf8e5aa0af
Antoine Pietri <antoine.pietri1@gmail.com>
2025-11-02 12:05:19 +0100

    weechat: start with --upgrade

--- roles/irc-client.nix
+++ roles/irc-client.nix
@@ -49,7 +49,7 @@ in
           Group = user.group;
           Environment = [ "TMUX_TMPDIR=/run/user/1000" ]; # aaaaa
           Type = "forking";
-          ExecStart = "${pkgs.tmux}/bin/tmux -2 new-session -d -s irc ${pkgs.weechat}/bin/weechat \\; set status off";
+          ExecStart = "${pkgs.tmux}/bin/tmux -2 new-session -d -s irc ${pkgs.weechat}/bin/weechat --upgrade \\; set status off";
           ExecStop = "${pkgs.tmux}/bin/tmux kill-session -t irc";
         };
         wantedBy = [ "multi-user.target" ];
