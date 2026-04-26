c8ce5beb4dd077775cab75eff33d788364fcbef5
Antoine Pietri <antoine.pietri1@gmail.com>
2026-04-26 16:36:15 +0200

    Update wezterm config/theme and make it the default

--- home/graphical.nix
+++ home/graphical.nix
@@ -12,6 +12,7 @@ in
     my.home.i3.enable = true;
     my.home.gnome.enable = true;
     my.home.alacritty.enable = true;
+    my.home.wezterm.enable = true;
     my.home.urxvt.enable = true;
     my.home.xcompose.enable = true;
     my.home.vscode.enable = true;
@@ -22,7 +23,7 @@ in
     services.autorandr.enable = lib.mkDefault true;
 
     home.sessionVariables = {
-      TERMINAL = "alacritty";
+      TERMINAL = "wezterm";
     };
   };
 }
--- home/wezterm.nix
+++ home/wezterm.nix
@@ -18,21 +18,23 @@ in
               "Noto Color Emoji",
               "Symbola",
           },
-          font_size = 9.0,
-          color_scheme = "Tango (terminal.sexy)",
+          window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0,
+          },
+          colors = {
+            cursor_bg = 'white',
+            cursor_border = 'white',
+          },
+          color_scheme = "Gnometerm (terminal.sexy)",
+          font_size = 8.5,
+          freetype_load_target = "Light",
           enable_tab_bar = false,
           window_close_confirmation = "NeverPrompt",
         }
       '';
-
-      # font = {
-      #   size = 8.5;
-      #   name = "DejaVu Sans Mono";
-      # };
-      # theme = "Tango Dark";
-      # settings = {
-      #   confirm_os_window_close = "0";
-      # };
     };
   };
 }
